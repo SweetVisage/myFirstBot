710479a0df16adaabc01226e816386cf6878022b
SweetVisage <hyedzen@yandex.ru>
2019-08-13 19:46:50 +0400

    еуые

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ bot = telebot.TeleBot(Token)
 
 @bot.message_handler(func=lambda message: True)
 def fn_message(message):
-    calc_message = len(message.text)
     if "привет" in message.text:
         bot.reply_to(message, "ТЫ ГДЕ ВОТ ЩАС?")
     elif "а ты?" in message.text:
         bot.reply_to(message, 'НА МЕМОРИАЛЕ')
+    else:
+        bot.reply_to(message, 'ЧЕ ТЫ НЕСЕШЬ, ЕБЛАН?')
 
 
 bot.polling()
